c8973c44e432cf57b9b14773e919c83cb2f786fa
Roman <krasivskyi01@gmail.com>
2026-04-19 18:15:44 +0300

    hm-js-6

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,20 @@
 class Storage {
+  #items;
+
   constructor(items) {
-    this.items = items || [];
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const item = this.items.indexOf(itemToRemove);
-    if (item > -1) {
-      this.items.splice(item, 1);
-    }
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 
